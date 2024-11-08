4eb134302de2e8631ee65646047b1287c78c81f1
Gustavo Gama <gustavo.gama@fit-tecnologia.org.br>
2024-11-08 11:17:42 -0300

    creates "Student" entity

---
+++ SchoolManager.Domain/Entities/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace SchoolManager.Domain.Entities;
+
+public class Student
+{
+    public Guid Id { get; private init; } = Guid.NewGuid();
+
+    public string Name { get; private set; } = string.Empty;
+
+    public int Age { get; private set; } = 0;
+}
