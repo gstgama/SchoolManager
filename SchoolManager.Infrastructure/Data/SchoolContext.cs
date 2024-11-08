84eae079454dd522ce65fa296c3454b8ece1962b
Gustavo Gama <gustavo.gama@fit-tecnologia.org.br>
2024-11-08 11:18:20 -0300

    creates DbContext file

---
+++ SchoolManager.Infrastructure/Data/SchoolContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolManager.Domain.Entities;
+
+namespace SchoolManager.Infrastructure.Data;
+
+public class SchoolContext(DbContextOptions options) : DbContext(options)
+{
+    //TODO: Colocar log de query
+
+    public DbSet<Student> Students { get; set; }
+}
