ed1223c3804c94fd9942a442961e51eb06587e2b
Gustavo Gama <gustavo.gama@fit-tecnologia.org.br>
2024-11-08 11:17:22 -0300

    adds database configs

---
+++ SchoolManager.Infrastructure/Data/SchoolContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace SchoolManager.Infrastructure.Data;
+
+public class SchoolContextFactory : IDesignTimeDbContextFactory<SchoolContext>
+{
+    public SchoolContext CreateDbContext(string[] args)
+    {
+        //TODO: Entender pq nao esta dando para rodar o comando dotnet ef database update
+        //IConfigurationRoot configuration = new ConfigurationBuilder()
+        //    .SetBasePath(Directory.GetCurrentDirectory())
+        //    .AddJsonFile(@"..\..\SchoolManager.API\appsettings.json")
+        //    .Build();
+
+        //TODO: Melhorar local da connection string
+        DbContextOptionsBuilder<SchoolContext> optionsBuilder = new();
+        optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=SchoolManager;Trusted_Connection=True;TrustServerCertificate=true;");
+
+        return new SchoolContext(optionsBuilder.Options);
+    }
+}
